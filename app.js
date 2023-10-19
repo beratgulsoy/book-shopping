c68329af136e9f47356e528ce0e19e4d5fb266eb
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-20 02:38:00 +0300

    learning express methods and added bodyparser

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const http = require('http');
-
 const express = require('express');
+const bp = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
+app.use(bp.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
+app.post('/product', (req, res, next) => {
+    console.log(req.body.title);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
     res.send('<h1>Hello</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);

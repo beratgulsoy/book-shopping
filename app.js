18513949731117733384d0ccd2a4d689a0cfd834
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-20 01:07:46 +0300

    split the code and add routes

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
- const http = require('http');
- const fs = require('fs');
+const http = require('http');
+
+const routes = require('./routes');
 
 
-
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.setHeader('Contont-Type', 'text/html');
-        res.write('<html><head><meta charset="UTF-8"/><title>Enter Message</title></head><body><form action="/message" method="POST"><input type="text" name="msg"><button onclick="submit">Gönder</button></form></body></html>');
-        return res.end();
-    }
-    if(url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString()
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end;
-            });
-        });
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html><head><meta chatset="UTF-8"/><title>Main Page</title></head><body><h1>Hello!</h1></body></html>');
-    res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);

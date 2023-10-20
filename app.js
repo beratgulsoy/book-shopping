8bf463e528eea121d18e7951c26dade567ce94e0
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-21 01:43:40 +0300

    added pug

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const bp = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -16,7 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).render('404');
 
 });
 

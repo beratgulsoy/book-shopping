3d1cf69abd29e3c9674f57f995b73b472f3cf4a0
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-20 03:46:46 +0300

    paths...

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
+const path = require('path');
+
 const express = require('express');
 const bp = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bp.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body.title);
-    res.redirect('/');
-});
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'));
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello</h1>');
 });
 
 app.listen(3000);

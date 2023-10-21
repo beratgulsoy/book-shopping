4b63ef33b86bf74c127e039c178314cf7a7f4127
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-21 04:18:25 +0300

    finished models

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const path = require('path');
 const express = require('express');
 const rootDir = require('./util/path');
 const bp = require('body-parser');
+const errorController = require('./controllers/error');
 
 const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,9 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not Found'});
-
-});
+app.use(errorController.get404);
 
 app.listen(3000);

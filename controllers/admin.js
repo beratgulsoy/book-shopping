ec1e5bc9a476eba2d1356392564cb19829462346
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-23 02:01:04 +0300

    added functionality to adminside edit product

--- controllers/admin.js
+++ controllers/admin.js
@@ -38,6 +38,21 @@ exports.getEditProduct = (req, res, next) => {
     
 };
 
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+    const updatedPrice = req.body.price;
+    const updatedProduct = new Product(prodId,
+        updatedTitle,
+        updatedImageUrl,
+        updatedDescription,
+        updatedPrice);
+    updatedProduct.save();
+    res.redirect('/');
+};
+
 exports.getProducts =  (req, res, next) => {
     Product.fetchAll(products => {
         res.render('admin/products', {

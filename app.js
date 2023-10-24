6c95037c1bcea1ae90fb50aebe091ac462158e13
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-24 03:20:54 +0300

    s

--- app.js
+++ app.js
@@ -16,14 +16,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products')
-.then(result => {
-    console.log(result[0]);
-})
-.catch(err => {
-    console.log(err);
-});
-
 app.use(bp.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 

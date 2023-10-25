efc6ffc4d0826d269bae0f776f275396b4175f1f
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-25 03:57:16 +0300

    changes on styling and selecting single data

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,9 +13,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
-    Product.findById(prodId).then(([product]) => {
+    //BU KOD YERİNE FİNDBYPK (where için önemli)
+    //Product.findAll({where: {id: prodId}}).then(burda arrow func içi render).catch();
+    Product.findByPk(prodId).then(product => {
         res.render('shop/product-detail', {
-            product: product[0],
+            product: product,
             pageTitle: product.title,
             path: '/products'
         });

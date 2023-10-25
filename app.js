d3b45a57281c1a44a2f70449d80e623537233fb8
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-25 03:25:49 +0300

    sequelize create table

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const rootDir = require('./util/path');
 const bp = require('body-parser');
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -23,5 +23,9 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize.sync().then(res => {
+    //console.log(res);
+}).catch(err => console.log(err));
 
 app.listen(3000);

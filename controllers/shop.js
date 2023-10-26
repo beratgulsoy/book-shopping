74a6620f77be15831055d96a429f6743e4a8fa75
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-26 04:21:35 +0300

    end of sql based db and sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -109,9 +109,11 @@ exports.postCart = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -127,25 +129,24 @@ exports.postOrder = (req, res, next) => {
         })
         .catch((err) => console.log(err));
     })
-    .then(result => {
-      res.redirect('/orders');
+    .then((result) => {
+      return fetchedCart.setProducts(null);
+    })
+    .then((result) => {
+      res.redirect("/orders");
     })
     .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-    res.render("shop/orders", {
-      pageTitle: "Your Orders",
-      path: "/orders",
-    });
-};
-
-exports.getCheckout = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/checkout", {
-      prods: products,
-      pageTitle: "Checkout",
-      path: "/checkout",
-    });
-  });
+  req.user
+    .getOrders({ include: ['products'] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        pageTitle: "Your Orders",
+        path: "/orders",
+        orders: orders
+      });
+    })
+    .catch((err) => console.log(err));
 };

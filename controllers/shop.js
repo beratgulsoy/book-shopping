cc59e0bd13ded136888936449c775ed887105979
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-28 02:49:33 +0300

    order view

--- controllers/shop.js
+++ controllers/shop.js
@@ -116,12 +116,12 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({ include: ['products'] })
+    .getOrders()
     .then((orders) => {
       res.render("shop/orders", {
         pageTitle: "Your Orders",
         path: "/orders",
-        orders: orders
+        orders: orders,
       });
     })
     .catch((err) => console.log(err));

524c9cf01a9de9c393533c2b1d1e5fc88f0dd535
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-28 01:46:44 +0300

    delete item from cart

--- routes/shop.js
+++ routes/shop.js
@@ -16,7 +16,7 @@ router.get("/cart", shopController.getCart);
 
 router.post("/cart", shopController.postCart);
 
-// router.post('/delete-cart-item', shopController.postCartDeleteProduct);
+router.post("/delete-cart-item", shopController.postCartDeleteProduct);
 
 // router.post('/create-order', shopController.postOrder);
 

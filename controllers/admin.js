755bc0926aa048338dcb9d019b4465ecf37d7671
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-30 01:27:23 +0300

    add to cart

--- controllers/admin.js
+++ controllers/admin.js
@@ -85,6 +85,7 @@ exports.postDeleteProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .populate('userId')
     .then((products) => {
       res.render("admin/products", {
         prods: products,

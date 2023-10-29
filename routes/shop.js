9bc8fec3298384d44f0839d3f06ab6d08584705f
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-30 02:34:17 +0300

    post order

--- routes/shop.js
+++ routes/shop.js
@@ -18,7 +18,7 @@ router.post("/cart", shopController.postCart);
 
 router.post("/delete-cart-item", shopController.postCartDeleteProduct);
 
-// router.post('/create-order', shopController.postOrder);
+router.post("/create-order", shopController.postOrder);
 
 // router.get("/orders", shopController.getOrders);
 

7db29005a220a434fcc6f63383121d5b99ebf084
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-31 02:22:55 +0300

    more sessions

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const rootDir = require("./util/path");
 const bp = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 
 const User = require("./models/user");
 
+const MONGODB_URI =
+  "mongodb+srv://berat:TyLHtt3Yxaa2DGWl@cluster0.2nxyobm.mongodb.net/shop?retryWrites=true&w=majority";
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -22,7 +30,12 @@ const authRoutes = require("./routes/auth");
 app.use(bp.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-  session({ secret: "my secret", resave: false, saveUninitialized: false })
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
 
 app.use((req, res, next) => {
@@ -41,9 +54,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://berat:TyLHtt3Yxaa2DGWl@cluster0.2nxyobm.mongodb.net/shop?retryWrites=true&w=majority"
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {

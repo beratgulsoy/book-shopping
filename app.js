1d0effb110a8a7341b5bd5d1b85da4c15a78e5a6
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-31 17:05:07 +0300

    signup

--- app.js
+++ app.js
@@ -59,18 +59,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Berat",
-          email: "berat@test.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => console.log(err));

1d66440e88f01d44eba503497059ed41449b1bba
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-31 17:17:56 +0300

    hash the password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcyrpt = require("bcryptjs");
+
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
@@ -42,9 +44,12 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.response("/signup");
       }
+      return bcyrpt.hash(password, 12);
+    })
+    .then((hashedPassword) => {
       const user = new User({
         email: email,
-        password: password,
+        password: hashedPassword,
         cart: { items: [] },
       });
       return user.save();

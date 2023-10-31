38b339d97b7c6d29c7d8fa24798deae799630585
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-31 03:53:13 +0300

    added sign up page

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,18 @@
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
   console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
+    isAuthenticated: false,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    pageTitle: "Sign Up!",
+    path: "/signup",
     isAuthenticated: false,
   });
 };
@@ -25,6 +32,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {

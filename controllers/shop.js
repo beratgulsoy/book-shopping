01b9fd5eb4b4a247b330b3245be3d9f4f935ec44
beratgulsoy <beratgulsoyy@gmail.com>
2023-10-31 03:24:52 +0300

    errorfix

--- controllers/shop.js
+++ controllers/shop.js
@@ -42,7 +42,7 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = async (req, res, next) => {
-  await req.session.user
+  await req.user
     .populate("cart.items.productId")
     .then((user) => {
       const products = user.cart.items;
@@ -58,7 +58,7 @@ exports.getCart = async (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.session.user
+  req.user
     .removeFromCart(prodId)
     .then((result) => {
       res.redirect("/cart");
@@ -70,7 +70,7 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then((product) => {
-      return req.session.user.addToCart(product);
+      return req.user.addToCart(product);
     })
     .then((result) => {
       console.log(result);
@@ -80,7 +80,7 @@ exports.postCart = (req, res, next) => {
 };
 
 exports.postOrder = async (req, res, next) => {
-  await req.session.user
+  await req.user
     .populate("cart.items.productId")
     .then((user) => {
       const products = user.cart.items.map((i) => {
@@ -88,15 +88,15 @@ exports.postOrder = async (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.session.user.name,
-          userId: req.session.user,
+          name: req.user.name,
+          userId: req.user,
         },
         products: products,
       });
       return order.save();
     })
     .then((result) => {
-      return req.session.user.clearCart();
+      return req.user.clearCart();
     })
     .then(() => {
       res.redirect("/orders");
@@ -105,7 +105,7 @@ exports.postOrder = async (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  Order.find({ "user.userId": req.session.user._id })
+  Order.find({ "user.userId": req.user._id })
     .then((orders) => {
       res.render("shop/orders", {
         pageTitle: "Your Orders",

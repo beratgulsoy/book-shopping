24fadaefeddcd0597e104abbef2b73a093496ed7
beratgulsoy <beratgulsoyy@gmail.com>
2023-11-02 21:45:58 +0300

    email validate on server side

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const crypto = require("crypto");
 const bcyrpt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 const nodemailer = require("nodemailer");
 
 const User = require("../models/user");
@@ -75,6 +76,14 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/signup", {
+      pageTitle: "Sign Up!",
+      path: "/signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {

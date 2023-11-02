d0aa27c5ebaaa0dd3559ae48f91c2691a580ae58
beratgulsoy <beratgulsoyy@gmail.com>
2023-11-02 22:21:24 +0300

    passw equality check

--- controllers/auth.js
+++ controllers/auth.js
@@ -75,7 +75,6 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).render("auth/signup", {
